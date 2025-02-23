28ef014113df78eee901007bea16cd5a5ed228f9
syedjaferk <learnwithjafer@gmail.com>
2025-02-23 18:47:13 +0530

    feat: baseurl for data.json

--- tracker/src/app/course-window/api.server.ts
+++ tracker/src/app/course-window/api.server.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class JsonApiService {
-  private apiUrl = '/data.json';
+  private apiUrl = '/tracker/data.json';
 
   constructor(private http: HttpClient) {}
 
